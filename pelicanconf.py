3753feec5fccb3700d3a8715d343c1abab061689
Eyob Woldeghebriel <eyobworrede@gmail.com>
2024-05-20 20:01:59 -0700

    A lot of changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Tian Lin'
-SITENAME = 'The Name of the Lin'
-SITEURL = 'https://tianl.in'
+SITENAME = 'Tian Lin'
+SITEURL = 'http://localhost:8000'
 
 PATH = 'content'
 
@@ -16,16 +16,24 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'https://getpelican.com/'),
+#         ('Python.org', 'https://www.python.org/'),
+#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+DISPLAY_PAGES_ON_MENU = True
 DEFAULT_PAGINATION = False
-
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+
+# Customization
+COLOR_THEME = '0a'
+SOCIAL = (('youtube', 'https://www.youtube.com/@tian_creator'),
+          ('instagram', 'https://www.instagram.com/tian_creator'),
+          ('linkedin', 'linkedin.com/in/tian-lin-mit'),)
+
+PROFILE_IMAGE = 'avatar.jpg'
+
+MENUITEMS = (('Home', '/'),)
+
